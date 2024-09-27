53b20e1adf7902715eb85e4e203144d3a9a18d86
Vishal962122 <vishal962122@gmail.com>
2024-09-27 19:42:21 +0530

    first node

---
+++ index.js
@@ -0,0 +1,16 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+app.get('/api',(req,res)=>{
+    res.send('Welcome to my API!')
+  
+})
+
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${port}`)
+})
